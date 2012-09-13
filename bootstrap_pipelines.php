318f71e410d9d79f9be084dac5cda0a3e5a26a72
christophe le drean <christophe@rouge-rose.net>
2012-09-13 18:22:06 +0200

    corrections

--- bootstrap_pipelines.php
+++ bootstrap_pipelines.php
@@ -1,7 +1,5 @@
 <?php
    if (!defined("_ECRIRE_INC_VERSION")) return;
-   // commentaire
-   // commentaire 2
 
    function bootstrap_insert_head($flux){
       $js = find_in_path('js/boostrap.min.js');
