ebb648bd44ad9af8238025e9f24feea43aa9dde6
christophe le drean <christophe@rouge-rose.net>
2012-09-28 11:01:25 +0200

    ajout caap_pipelines et fichier de langue

---
+++ plugins/caap/caap_pipelines.php
@@ -0,0 +1,19 @@
+<?php
+   if (!defined("_ECRIRE_INC_VERSION")) return;
+
+   // javascript
+   function caap_insert_head($flux){
+      $js = find_in_path('javascript/caap_public.js');
+      if ($js) {
+         $flux .= "\n".'<script src="'.$js.'" type="text/javascript"></script>'."\n";
+      }
+      return $flux;
+   }
+
+   // css
+   function caap_insert_head_css($flux) {
+       $flux .= "<link href='http://fonts.googleapis.com/css?family=Cabin:700italic&text=Comit%C3%A9%20des%20Artistes-Auteurs%20Plasticiens' rel='stylesheet' type='text/css'>"."\n";
+       $flux .= "<link href='http://fonts.googleapis.com/css?family=Cabin:400,700|Tinos:400,700,400italic,700italic' rel='stylesheet' type='text/css'>"."\n";
+       return $flux;
+   }
+?>
