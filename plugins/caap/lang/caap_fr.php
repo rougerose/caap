75d6d02ad3721e35700c3cf981e696b8af3c262d
christophe le drean <christophe@rouge-rose.net>
2012-09-30 23:02:27 +0200

    article (work in progress)

--- plugins/caap/lang/caap_fr.php
+++ plugins/caap/lang/caap_fr.php
@@ -4,7 +4,15 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+   // I
+   'info_ps' => 'Post-scriptum',
+
    //P
+   'pager_precedent' => '&#8249;',
+   'pager_precedent_info' => 'Article précédent',
+   'pager_suivant' => '&#8250;',
+   'pager_suivant_info' => 'Article suivant',
    'par' => 'par',
 
    // S
