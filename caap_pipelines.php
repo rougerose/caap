2c63f6f3e60ad1021d0af4b59f82e7c81a9b935b
christophe le drean <christophe@rouge-rose.net>
2012-11-01 12:40:03 +0100

    on reprend...

--- caap_pipelines.php
+++ caap_pipelines.php
@@ -1,19 +1,34 @@
 <?php
-   if (!defined("_ECRIRE_INC_VERSION")) return;
 
-   // javascript
-   function caap_insert_head($flux){
-      $js = find_in_path('javascript/caap_public.js');
-      if ($js) {
-         $flux .= "\n".'<script src="'.$js.'" type="text/javascript"></script>'."\n";
-      }
-      return $flux;
-   }
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
-   // css
-   function caap_insert_head_css($flux) {
-      $flux .= "<link href='http://fonts.googleapis.com/css?family=Cabin:700italic&text=Comit%C3%A9%20des%20Artistes-Auteurs%20Plasticiens' rel='stylesheet' type='text/css' />"."\n";
-      $flux .= "<link href='http://fonts.googleapis.com/css?family=Cabin:400,700,400italic|Tinos:400,700,400italic,700italic' rel='stylesheet' type='text/css' />"."\n";
-       return $flux;
+// javascript
+function caap_insert_head($flux){
+   $js = find_in_path('js/caap_public.js');
+   if ($js) {
+      $flux .= "\n".'<script src="'.$js.'" type="text/javascript"></script>'."\n";
    }
+   return $flux;
+}
+
+// css
+function caap_insert_head_css($flux) {
+   return $flux;
+}
+
+function caap_affichage_final($flux){
+   if (
+   $GLOBALS['html']
+   AND isset($GLOBALS['visiteur_session']['statut'])
+   AND $GLOBALS['visiteur_session']['statut']=='0minirezo'
+   AND $GLOBALS['visiteur_session']['webmestre']=='oui'
+   AND $p=stripos($flux,"</body>")
+   AND $f = find_in_path("js/hashgrid.js")
+   ){
+      $flux = substr_replace($flux,'<script type="text/javascript" src="'.$f.'"></script>',$p,0);
+   }
+   return $flux;
+}
+
+
 ?>
