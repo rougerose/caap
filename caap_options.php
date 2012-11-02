7a4257d4feeb60766aa91f8c9c132a1d08576afc
christophe le drean <christophe@rouge-rose.net>
2012-11-02 17:09:00 +0100

    pas de cache pour le développement

--- caap_options.php
+++ caap_options.php
@@ -7,6 +7,9 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+// en dev, pas de cache
+define('_NO_CACHE', 1);
+
 $GLOBALS['debut_intertitre'] = "\n<h2 class='spip'>";
 $GLOBALS['fin_intertitre'] = "</h2>\n";
 
