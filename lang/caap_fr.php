44049bfc511ca1dc26a22bffa1bd1d3f555e7f2e
christophe le drean <christophe@rouge-rose.net>
2012-11-07 18:13:46 +0100

    suite : - recherche : squelette et formulaire. Le formulaire est à placer définitivement. - logos d'article et de rubrique

--- lang/caap_fr.php
+++ lang/caap_fr.php
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    // D
    'info_document' => 'Document',
    'info_documents' => 'Documents',
+   'info_rechercher' => 'Rechercher',
 
    // I
    'info_ps' => 'Post-scriptum',
@@ -18,6 +19,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
    'pager_suivant' => '&#8250;',
    'pager_suivant_info' => 'Article suivant',
    'par' => 'par',
+
+   // R
+   'recherche_article' => '1 article',
+   'recherche_articles' => '@nb@ articles',
+   'recherche_document' => '1 document',
+   'recherche_documents' => '@nb@ documents',
+   'recherche_label' => 'Recherche',
+   'recherche_label_cap' => 'RECHERCHE',
+   'recherche_mot' => '1 mot-clé',
+   'recherche_mots' => '@nb@ mots',
+   'recherche_rubrique' => '1 rubrique',
+   'recherche_rubriques' => '@nb@ rubriques',
+   'recherche_site' => '1 site web',
+   'recherche_sites' => '@nb@ sites web',
+   'recherche_titre' => 'Recherche :',
+   'recherche_resultat' => '1 résultat',
+   'recherche_resultats' => '@nb@ résultats',
+   'recherche_pas_de_resultat' => 'Aucun résultat',
+
 
    // S
    'suite' => 'Lire la suite'
