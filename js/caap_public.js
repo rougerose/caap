bebd0cf41756a17f4923a4a9335a522deb990e36
christophe le drean <christophe@rouge-rose.net>
2012-11-09 13:04:11 +0100

    suppression

--- js/caap_public.js
+++ js/caap_public.js
@@ -2,21 +2,11 @@ $(document).ready(function() {
    // ==============================
    // = descriptif site, animation =
    // ==============================
-       $('#ds .append').animateAppend();
+   $('#ds .append').animateAppend();
 
    // =============================================
    // = jquery ui accordion / rubrique Ressources =
    // =============================================
-   /*$("#ressources").accordion({
-      header:'dt',
-      autoHeight: false,
-      icons:false,
-      navigation:true
-      // on se met doucement là où il faut http://stackoverflow.com/a/3621845
-      //change: function(event,ui) {
-      //   $.scrollTo(ui.newHeader,500);
-      //}
-   });*/
    $("#ressources").accordion({
       header: 'dt',
       active:false,
