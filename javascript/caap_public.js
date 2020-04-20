5f4defd04e81aced4e8bac531f43ea01d5705c0d
christophe le drean <christophe@rouge-rose.net>
2020-04-20 16:35:30 +0200

    Adaptations pour Jquery UI pour spip 3.2 :
    - désactiver les styles UI par défaut dans la partie publique et
    utiliser uniquement ceux du plugin Caap.

--- javascript/caap_public.js
+++ javascript/caap_public.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-   // ==============================
-   // = descriptif site, animation =
-   // ==============================
-   $('#ds .append').animateAppend();
+$(document).ready(function () {
+  // ==============================
+  // = descriptif site, animation =
+  // ==============================
+  $("#ds .append").animateAppend();
 
-   // =============================================
-   // = jquery ui accordion / rubrique Ressources =
-   // =============================================
-   $("#ressources").accordion({
-     header: '.ressources-header',
-     autoHeight: false
-   });
+  // =============================================
+  // = jquery ui accordion / rubrique Ressources =
+  // =============================================
+  $("#ressources").accordion({
+    header: ".ressources-header",
+    heightStyle: "content",
+    // autoHeight: false
+  });
 
-   // ==================================
-   // = Animation du panneau-recherche =
-   // ==================================
-   $(".recherche-panneau .action").click(function(){
-      $("#formulaire_recherche").slideToggle('slow','swing');
-      $(this).toggleClass("on");
-   });
-	// le formulaire est affiché par défaut sur la page de recherche
-	if ($("html.page_recherche").length) { $(".recherche-panneau .action").trigger("click"); }
-
+  // ==================================
+  // = Animation du panneau-recherche =
+  // ==================================
+  $(".recherche-panneau .action").click(function () {
+    $("#formulaire_recherche").slideToggle("slow", "swing");
+    $(this).toggleClass("on");
+  });
+  // le formulaire est affiché par défaut sur la page de recherche
+  if ($("html.page_recherche").length) {
+    $(".recherche-panneau .action").trigger("click");
+  }
 });
